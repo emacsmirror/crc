265c6cf90a53ff858ab80d2ed5068ed36a224bcf
Jean Libète <tomenzgg@mail.mayfirst.org>
2024-03-08 22:28:23 -0600

    We'll need this to use ~seq-reduce~ and =seq= will necessitate v25.1, at least.

--- crc-32.el
+++ crc-32.el
@@ -4,6 +4,7 @@
 
 ;; Author: Jean Libète <tomenzgg@mail.mayfirst.org>
 ;; URL: https://codeberg.org/tomenzgg/Emacs-CRC-32
+;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: lisp, checksum, algorithms
 ;; Version: 1.0.0
 
@@ -25,6 +26,9 @@
 ;; Library implementing the checksum algorithm Cyclic Redundancy Check 32.
 
 ;;; Code:
+;; seq-reduce
+(require 'seq)
+
 
 (provide 'crc-32)
 
