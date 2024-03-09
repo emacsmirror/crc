4256096ce2a733f127ad3558f867cf7ab6a788a2
Jean Libète <tomenzgg@mail.mayfirst.org>
2024-03-08 21:22:49 -0600

    The usual boilerplate.

---
+++ crc-32.el
@@ -0,0 +1,31 @@
+;;; crc-32.el --- CRC-32                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Jean Libète
+
+;; Author: Jean Libète <tomenzgg@mail.mayfirst.org>
+;; URL: https://codeberg.org/tomenzgg/Emacs-CRC-32
+;; Keywords: lisp, checksum, algorithms
+;; Version: 1.0.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published
+;; by the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Library implementing the checksum algorithm Cyclic Redundancy Check 32.
+
+;;; Code:
+
+(provide 'crc-32)
+
+;;; crc-32.el ends here
