97865782cbd73d918660588aa45a76383b29fbb1
Jean Libète <tomenzgg@mail.mayfirst.org>
2024-03-10 22:57:29 -0500

    I'm still going to see if I can get the ones less than 8 but let's add this, first.

--- crc.el
+++ crc.el
@@ -25,6 +25,9 @@
 
 ;; Library implementing checksum algorithm Cyclic Redundancy Check variations.
 
+;; All references to documentation/implementations are thanks to the Catalogue
+;; of Parametrised CRC Algorithms: https://reveng.sourceforge.io/crc-catalogue/
+
 ;;; Code:
 ;; seq-reduce
 (require 'seq)
