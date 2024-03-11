785e0aa433d4ee1a5a0a6fcf59ac39915c58cc43
Jaft <jaft.r@mail.mayfirst.org>
2024-03-10 23:34:46 -0500

    'Learned what these acronyms are for so let's reflect that in the documentation.

--- crc.el
+++ crc.el
@@ -45,8 +45,8 @@
   "General Cyclic Redundancy Check application with customizations (via arg.s).
 
 Because there are varying versions of CRC – depending on the Number of Bits,
-Polynomial, Initialization value, RefIn and RefOut, and the XorOut –, this
-function serves to handle any possible iteration that needs to be computed.
+Polynomial, Initialization value, Reflect-In and Reflect-Out, and the XorOut –,
+this function serves to handle any possible iteration that needs to be computed.
 
 NUMBER-OF-BITS is an integer (such as 8, 16, 32, etc.).
 
