1421d72b1a97ed3ade0a05709a3ddc6432b889c4
Jean Libète <tomenzgg@mail.mayfirst.org>
2024-03-16 03:33:10 -0500

    Revert "I see other libraries, added to ELPA, which do this so I'm guessing I do, too."
    This reverts commit 79c5c38f0097f4631cac1ee6e6ebefc55bd57ee8.
    
    Emacs never got back to me so we're going to Melpa; shame but, well,
    maybe this is why an E-mail based flow sucks.

--- crc.el
+++ crc.el
@@ -1,6 +1,6 @@
 ;;; crc.el --- Cyclic Redundancy Check               -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024 Free Software Foundation, Inc.
+;; Copyright (C) 2024  Jean Libète
 
 ;; Author: Jean Libète <tomenzgg@mail.mayfirst.org>
 ;; Maintainer: Jean Libète <tomenzgg@mail.mayfirst.org>
@@ -9,20 +9,18 @@
 ;; Keywords: lisp, checksum, algorithms
 ;; Version: 1.0.0
 
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software: you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published
+;; by the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
