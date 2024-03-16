7b2b971bff9d693a2030820725b9a868c410394e
Jean Libète <tomenzgg@mail.mayfirst.org>
2024-03-16 13:25:37 -0500

    And change this to match =crc.el=, as well.

--- crc-tests.el
+++ crc-tests.el
@@ -1,25 +1,23 @@
 ;;; crc-tests.el --- Cyclic Redundancy Check's tests  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2024 Free Software Foundation, Inc.
+;; Copyright (C) 2024  Jean Libète
 
-;; Author: Jaft <jaft.r@mail.mayfirst.org>
-;; Maintainer: Jaft <jaft.r@mail.mayfirst.org>
+;; Author: Jean Libète <tomenzgg@mail.mayfirst.org>
+;; Maintainer: Jean Libète <tomenzgg@mail.mayfirst.org>
 ;; Keywords: tests
 
-;; This file is part of GNU Emacs.
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published
+;; by the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; This program is free software: you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation, either version 3 of the
-;; License, or (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see `http://www.gnu.org/licenses/'.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
